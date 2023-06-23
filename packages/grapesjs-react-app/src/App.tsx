f528a3e295916781cbb2745bc02b34a80590b8db
Artur Arseniev <artur.catch@hotmail.it>
2023-06-23 12:33:19 +0400

    Up app

--- packages/grapesjs-react-app/src/App.tsx
+++ packages/grapesjs-react-app/src/App.tsx
@@ -3,11 +3,14 @@ import type { Editor, ProjectData } from 'grapesjs';
 import { useCallback, useState } from 'react';
 import DefaultEditor from './examples/DefaultEditor';
 import { getDateString } from './examples/common';
+import Icon from '@mdi/react';
+import { mdiCheckBold, mdiClose } from '@mdi/js';
 
 function App() {
   const [editor, setEditor] =  useState<Editor>();
   const [projectData, setProjectData] =  useState<ProjectData>();
   const [projectDataDate, setProjectDataDate] =  useState<Date>();
+  const iconCls = `inline-block ${editor ? 'text-green-400' : 'text-red-400'}`;
 
   const onProjectUpdate = useCallback<Required<EditorProps>['onUpdate']>((pd) => {
     setProjectData(pd);
@@ -23,7 +26,14 @@ function App() {
         <div>
           Example
         </div>
-        <div>Editor loaded: {editor ? 'Y' : 'N'}</div>
+        <div className="flex gap-2 items-center">
+            Editor loaded:
+            <Icon
+              size={0.7}
+              path={editor ? mdiCheckBold : mdiClose}
+              className={iconCls}
+            />
+          </div>
         {
           !!projectDataDate &&
           <div>Editor last update: {getDateString(projectDataDate)}</div>
