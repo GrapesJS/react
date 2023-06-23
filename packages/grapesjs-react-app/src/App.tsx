0b55d973106099f96e04387a7175d09108e06923
Artur Arseniev <artur.catch@hotmail.it>
2023-06-23 12:22:41 +0400

    Up

--- packages/grapesjs-react-app/src/App.tsx
+++ packages/grapesjs-react-app/src/App.tsx
@@ -4,14 +4,12 @@ import { useCallback, useState } from 'react';
 import DefaultEditor from './examples/DefaultEditor';
 import { getDateString } from './examples/common';
 
-type onUpdateProp = Required<EditorProps>['onUpdate'];
-
 function App() {
   const [editor, setEditor] =  useState<Editor>();
   const [projectData, setProjectData] =  useState<ProjectData>();
   const [projectDataDate, setProjectDataDate] =  useState<Date>();
 
-  const onProjectUpdate = useCallback<onUpdateProp>((pd) => {
+  const onProjectUpdate = useCallback<Required<EditorProps>['onUpdate']>((pd) => {
     setProjectData(pd);
     setProjectDataDate(new Date());
     projectData;
@@ -22,7 +20,9 @@ function App() {
   return (
     <div className="flex flex-col h-screen  text-sm text-white bg-slate-900">
       <div className="flex gap-5 p-3">
-        <div>Example</div>
+        <div>
+          Example
+        </div>
         <div>Editor loaded: {editor ? 'Y' : 'N'}</div>
         {
           !!projectDataDate &&
