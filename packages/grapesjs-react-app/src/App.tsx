3bb80ec74a3de756be2ee6d5bd994812c3897d0d
Artur Arseniev <artur.catch@hotmail.it>
2023-06-23 14:13:46 +0400

    Up App

--- packages/grapesjs-react-app/src/App.tsx
+++ packages/grapesjs-react-app/src/App.tsx
@@ -1,15 +1,21 @@
 import { EditorProps } from '@grapesjs/react';
 import type { Editor, ProjectData } from 'grapesjs';
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import DefaultEditor from './examples/DefaultEditor';
 import { getDateString } from './examples/common';
 import Icon from '@mdi/react';
 import { mdiCheckBold, mdiClose } from '@mdi/js';
 
+enum Examples {
+  Default = 'Default Editor',
+  Custom = 'Custom UI Editor',
+}
+
 function App() {
   const [editor, setEditor] =  useState<Editor>();
   const [projectData, setProjectData] =  useState<ProjectData>();
   const [projectDataDate, setProjectDataDate] =  useState<Date>();
+  const [selectedExample, setSelectedExample] =  useState(Examples.Default);
   const iconCls = `inline-block ${editor ? 'text-green-400' : 'text-red-400'}`;
 
   const onProjectUpdate = useCallback<Required<EditorProps>['onUpdate']>((pd) => {
@@ -18,13 +24,25 @@ function App() {
     projectData;
   }, []);
 
-  console.log('App GrapesJS');
+  const exampleOptions = useMemo(() => (
+    Object.keys(Examples).map((key) => (
+      <option key={key} value={key}>
+        {Examples[key as keyof typeof Examples]}
+      </option>
+    ))
+  ), []);
 
   return (
     <div className="flex flex-col h-screen  text-sm text-white bg-slate-900">
       <div className="flex gap-5 p-3">
         <div>
-          Example
+          <select
+            className="rounded-sm bg-slate-700 p-1"
+            value={selectedExample}
+            onChange={(ev) => setSelectedExample(ev.target.value as Examples)}
+          >
+            {exampleOptions}
+          </select>
         </div>
         <div className="flex gap-2 items-center">
             Editor loaded:
@@ -36,7 +54,7 @@ function App() {
           </div>
         {
           !!projectDataDate &&
-          <div>Editor last update: {getDateString(projectDataDate)}</div>
+          <div>Last update: {getDateString(projectDataDate)}</div>
         }
       </div>
       <div className="flex-grow">
