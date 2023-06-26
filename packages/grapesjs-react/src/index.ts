b0b2a9ca842287bc4ff48ebf63f4e1a5b5a0c54c
Artur Arseniev <artur.catch@hotmail.it>
2023-06-26 13:39:48 +0400

    Export AssetsProvider

--- packages/grapesjs-react/src/index.ts
+++ packages/grapesjs-react/src/index.ts
@@ -1,5 +1,6 @@
 export { default as Editor } from './Editor';
 export { default as Canvas } from './Canvas';
 export { default as ModalProvider } from './ModalProvider';
+export { default as AssetsProvider } from './AssetsProvider';
 export { default } from './Editor';
 export type * from './EditorInstance';
