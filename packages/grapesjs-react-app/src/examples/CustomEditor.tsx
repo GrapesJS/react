2374e31387ad8d1a4ee37e173dd64ae793c10107
Artur Arseniev <artur.catch@hotmail.it>
2023-06-27 15:03:47 +0400

    Add RightSidebar components

--- packages/grapesjs-react-app/src/examples/CustomEditor.tsx
+++ packages/grapesjs-react-app/src/examples/CustomEditor.tsx
@@ -1,14 +1,9 @@
-import GrapesJsEditor, { AssetsProvider, BlocksProvider, Canvas, EditorProps, ModalProvider, WithEditor } from '@grapesjs/react';
+import GrapesJsEditor, { AssetsProvider, Canvas, EditorProps, ModalProvider } from '@grapesjs/react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { defaultEditorProps } from './common';
-import CustomModal from './components/CustomModal';
 import CustomAssetManager from './components/CustomAssetManager';
-import CustomBlockManager from './components/CustomBlockManager';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import { useState } from 'react';
-import { mdiBrush, mdiLayers, mdiViewGridPlus } from '@mdi/js';
-import Icon from '@mdi/react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CustomModal from './components/CustomModal';
+import RightSidebar from './components/RightSidebar';
 
 const theme = createTheme({
     palette: {
@@ -20,11 +15,6 @@ const theme = createTheme({
 const clsBorderColor = 'border-slate-500';
 
 export default function CustomEditor(props: Partial<EditorProps>) {
-    const [selectedTab, setSelectedTab] = useState(0);
-    const handleTabChange = (_: React.SyntheticEvent, newValue: number) => {
-        setSelectedTab(newValue);
-    };
-
     return (
         <ThemeProvider theme={theme}>
             <GrapesJsEditor
@@ -33,34 +23,13 @@ export default function CustomEditor(props: Partial<EditorProps>) {
                 {...props}
             >
                 <div className={`flex h-full border-t ${clsBorderColor}`}>
-                    <div className={`gjs-column-l w-[300px] border-r ${clsBorderColor}`}>
-                        A
-                    </div>
                     <div className="gjs-column-m flex flex-col flex-grow">
                         <div className=" flex-grow">
                             Top
                         </div>
                         <Canvas className="flex-grow gjs-custom-editor-canvas"/>
                     </div>
-                    <div className={`overflow-y-auto gjs-column-r w-[300px] border-l ${clsBorderColor}`}>
-                        <Tabs value={selectedTab} onChange={handleTabChange} variant="fullWidth">
-                            <Tab label={<Icon size={1} path={mdiBrush}/>}/>
-                            <Tab label={<Icon size={1} path={mdiLayers}/>}/>
-                            <Tab label={<Icon size={1} path={mdiViewGridPlus}/>}/>
-                        </Tabs>
-                        {selectedTab === 0 && <div>Styles</div>}
-                        {selectedTab === 1 && <div>Layers</div>}
-                        {selectedTab === 2 && <div>Blocks</div>}
-                        <BlocksProvider>
-                            {({ mapCategoryBlocks, dragStart, dragStop }) => (
-                                <CustomBlockManager
-                                    mapCategoryBlocks={mapCategoryBlocks}
-                                    dragStart={dragStart}
-                                    dragStop={dragStop}
-                                />
-                            )}
-                        </BlocksProvider>
-                    </div>
+                    <RightSidebar className={`gjs-column-r w-[300px] border-l ${clsBorderColor}`}/>
                 </div>
                 <ModalProvider>
                     {({ open, title, content, close }) => (
---
+++ packages/grapesjs-react-app/src/examples/components/RightSidebar.tsx
@@ -0,0 +1,41 @@
+import { BlocksProvider } from '@grapesjs/react';
+import { mdiBrush, mdiLayers, mdiViewGridPlus } from '@mdi/js';
+import Icon from '@mdi/react';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import { useState } from 'react';
+import CustomBlockManager from './CustomBlockManager';
+import { cx } from '../common';
+
+export default function RightSidebar({ className }: React.HTMLAttributes<HTMLDivElement>) {
+    const [selectedTab, setSelectedTab] = useState(0);
+    const handleTabChange = (_: React.SyntheticEvent, newValue: number) => {
+        setSelectedTab(newValue);
+    };
+
+    return (
+      <div className={cx('gjs-right-sidebar flex flex-col', className)}>
+        <Tabs value={selectedTab} onChange={handleTabChange} variant="fullWidth">
+            <Tab label={<Icon size={1} path={mdiBrush}/>}/>
+            <Tab label={<Icon size={1} path={mdiLayers}/>}/>
+            <Tab label={<Icon size={1} path={mdiViewGridPlus}/>}/>
+        </Tabs>
+        <div className="overflow-y-auto flex-grow">
+            {selectedTab === 0 && <div>Styles</div>}
+            {selectedTab === 1 && <div>Layers</div>}
+            {
+                selectedTab === 2 &&
+                <BlocksProvider>
+                    {({ mapCategoryBlocks, dragStart, dragStop }) => (
+                        <CustomBlockManager
+                            mapCategoryBlocks={mapCategoryBlocks}
+                            dragStart={dragStart}
+                            dragStop={dragStop}
+                        />
+                    )}
+                </BlocksProvider>
+            }
+        </div>
+      </div>
+    );
+  }
