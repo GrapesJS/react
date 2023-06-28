0d3cf6a604169f7eed9109562364dc5b84da5d02
Artur Arseniev <artur.catch@hotmail.it>
2023-06-28 12:16:23 +0400

    Up RightSidebar

--- packages/grapesjs-react-app/src/examples/components/RightSidebar.tsx
+++ packages/grapesjs-react-app/src/examples/components/RightSidebar.tsx
@@ -1,5 +1,5 @@
 import { BlocksProvider } from '@grapesjs/react';
-import { mdiBrush, mdiLayers, mdiViewGridPlus } from '@mdi/js';
+import { mdiBrush, mdiLayers, mdiViewGridPlus, mdiTextBoxMultiple } from '@mdi/js';
 import Icon from '@mdi/react';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
@@ -7,15 +7,20 @@ import { useState } from 'react';
 import CustomBlockManager from './CustomBlockManager';
 import { cx } from '../common';
 
+const defaultTabProps = {
+    className: '!min-w-0',
+}
+
 export default function RightSidebar({ className }: React.HTMLAttributes<HTMLDivElement>) {
     const [selectedTab, setSelectedTab] = useState(0);
 
     return (
       <div className={cx('gjs-right-sidebar flex flex-col', className)}>
         <Tabs value={selectedTab} onChange={(_, v) => setSelectedTab(v)} variant="fullWidth">
-            <Tab label={<Icon size={1} path={mdiBrush}/>}/>
-            <Tab label={<Icon size={1} path={mdiLayers}/>}/>
-            <Tab label={<Icon size={1} path={mdiViewGridPlus}/>}/>
+            <Tab {...defaultTabProps} label={<Icon size={1} path={mdiBrush}/>}/>
+            <Tab {...defaultTabProps} label={<Icon size={1} path={mdiLayers}/>}/>
+            <Tab {...defaultTabProps} label={<Icon size={1} path={mdiViewGridPlus}/>}/>
+            <Tab {...defaultTabProps} label={<Icon size={1} path={mdiTextBoxMultiple}/>}/>
         </Tabs>
         <div className="overflow-y-auto flex-grow">
             {selectedTab === 0 && <div>Styles</div>}
