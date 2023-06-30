74594191f27fd7a615ef743b7045090cae51cdbe
Artur Arseniev <artur.catch@hotmail.it>
2023-06-30 14:23:47 +0400

    Up CustomStyleManager

--- packages/grapesjs-react-app/src/examples/components/CustomStyleManager.tsx
+++ packages/grapesjs-react-app/src/examples/components/CustomStyleManager.tsx
@@ -1,10 +1,10 @@
 import { StylesResultProps } from '@grapesjs/react';
-import { mdiMenuDown } from '@mdi/js';
+import { mdiClose, mdiMenuDown } from '@mdi/js';
 import Icon from '@mdi/react';
 import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
-import { MAIN_BG_COLOR } from '../common';
+import { MAIN_BG_COLOR, cx } from '../common';
 
 const accordionIcon = <Icon path={mdiMenuDown} size={0.7}/>;
 
@@ -17,8 +17,20 @@ export default function CustomStyleManager({ sectors }: StylesResultProps) {
                 <AccordionSummary className="!bg-slate-800" expandIcon={accordionIcon}>
                     { sector.getName() }
                 </AccordionSummary>
-                <AccordionDetails className={`!${MAIN_BG_COLOR}`}>
-                    Details: { sector.getName() }
+                <AccordionDetails className={`!${MAIN_BG_COLOR} flex flex-wrap`}>
+                    {sector.getProperties().map(prop => (
+                        <div key={prop.getId()} className={cx('mb-3', prop.isFull() ? 'w-full' : 'w-1/2')}>
+                            <div className="flex">
+                                <div className="flex-grow capitalize">{ prop.getName() }</div>
+                                {
+                                    prop.canClear() &&
+                                    <button onClick={() => prop.clear()}>
+                                        <Icon path={mdiClose} size={0.7}/>
+                                    </button>
+                                }
+                            </div>
+                        </div>
+                    ))}
                 </AccordionDetails>
             </Accordion>
         ))}
