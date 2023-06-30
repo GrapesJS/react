1beecbb99763c7afca74bf99a98907e827fac235
Artur Arseniev <artur.catch@hotmail.it>
2023-06-30 14:43:41 +0400

    Add TextField to CustomStyleManager

--- packages/grapesjs-react-app/src/examples/components/CustomStyleManager.tsx
+++ packages/grapesjs-react-app/src/examples/components/CustomStyleManager.tsx
@@ -4,6 +4,7 @@ import Icon from '@mdi/react';
 import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
+import TextField from '@mui/material/TextField';
 import { MAIN_BG_COLOR, cx } from '../common';
 
 const accordionIcon = <Icon path={mdiMenuDown} size={0.7}/>;
@@ -19,8 +20,8 @@ export default function CustomStyleManager({ sectors }: StylesResultProps) {
                 </AccordionSummary>
                 <AccordionDetails className={`!${MAIN_BG_COLOR} flex flex-wrap`}>
                     {sector.getProperties().map(prop => (
-                        <div key={prop.getId()} className={cx('mb-3', prop.isFull() ? 'w-full' : 'w-1/2')}>
-                            <div className="flex">
+                        <div key={prop.getId()} className={cx('mb-3 px-1', prop.isFull() ? 'w-full' : 'w-1/2')}>
+                            <div className="flex mb-2">
                                 <div className="flex-grow capitalize">{ prop.getName() }</div>
                                 {
                                     prop.canClear() &&
@@ -29,6 +30,16 @@ export default function CustomStyleManager({ sectors }: StylesResultProps) {
                                     </button>
                                 }
                             </div>
+                            {
+                                prop.getType() === 'number' &&
+                                <TextField
+                                    placeholder={prop.getDefaultValue()}
+                                    value={prop.hasValue() ? prop.getValue() : ''}
+                                    onChange={(ev) => {prop.upValue(ev.target.value)}}
+                                    size="small"
+                                />
+
+                            }
                         </div>
                     ))}
                 </AccordionDetails>
