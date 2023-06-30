7340dd60ec9ed5a3adc91f123bf324113d844ec1
Artur Arseniev <artur.catch@hotmail.it>
2023-06-30 15:01:39 +0400

    Add radio type

--- packages/grapesjs-react-app/src/examples/components/CustomStyleManager.tsx
+++ packages/grapesjs-react-app/src/examples/components/CustomStyleManager.tsx
@@ -4,7 +4,11 @@ import Icon from '@mdi/react';
 import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
 import TextField from '@mui/material/TextField';
+import type { PropertyRadio } from 'grapesjs';
 import { MAIN_BG_COLOR, cx } from '../common';
 
 const accordionIcon = <Icon path={mdiMenuDown} size={0.7}/>;
@@ -21,7 +25,7 @@ export default function CustomStyleManager({ sectors }: StylesResultProps) {
                 <AccordionDetails className={`!${MAIN_BG_COLOR} flex flex-wrap`}>
                     {sector.getProperties().map(prop => (
                         <div key={prop.getId()} className={cx('mb-3 px-1', prop.isFull() ? 'w-full' : 'w-1/2')}>
-                            <div className="flex mb-2">
+                            <div className={cx('flex mb-2', prop.canClear() && 'text-sky-300')}>
                                 <div className="flex-grow capitalize">{ prop.getName() }</div>
                                 {
                                     prop.canClear() &&
@@ -39,6 +43,19 @@ export default function CustomStyleManager({ sectors }: StylesResultProps) {
                                     size="small"
                                 />
 
+                            }
+                            {
+                                prop.getType() === 'radio' &&
+                                <RadioGroup value={prop.getValue()} onChange={(ev) => prop.upValue(ev.target.value)} row>
+                                    {(prop as PropertyRadio).getOptions().map(option => (
+                                        <FormControlLabel
+                                            key={(prop as PropertyRadio).getOptionId(option)}
+                                            value={(prop as PropertyRadio).getOptionId(option)}
+                                            label={(prop as PropertyRadio).getOptionLabel(option)}
+                                            control={<Radio size="small"/>}
+                                        />
+                                    ))}
+                                </RadioGroup>
                             }
                         </div>
                     ))}
