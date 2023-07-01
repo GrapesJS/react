0b13de820a5883ef1641b4ff40549a3dea345fd0
Artur Arseniev <artur.catch@hotmail.it>
2023-07-01 06:27:31 +0400

    Add stack type

--- packages/grapesjs-react-app/src/examples/components/StylePropertyField.tsx
+++ packages/grapesjs-react-app/src/examples/components/StylePropertyField.tsx
@@ -1,7 +1,9 @@
-import { mdiClose } from "@mdi/js";
+import { useEditor } from "@grapesjs/react";
+import { mdiArrowDownDropCircle, mdiArrowUpDropCircle, mdiClose, mdiDelete, mdiPlus } from "@mdi/js";
 import Icon from "@mdi/react";
 import FormControl from "@mui/material/FormControl";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import IconButton from "@mui/material/IconButton";
 import InputAdornment from "@mui/material/InputAdornment";
 import MenuItem from "@mui/material/MenuItem";
 import Radio from "@mui/material/Radio";
@@ -9,9 +11,8 @@ import RadioGroup from "@mui/material/RadioGroup";
 import Select from "@mui/material/Select";
 import Slider from "@mui/material/Slider";
 import TextField from "@mui/material/TextField";
-import type { Property, PropertyComposite, PropertyRadio, PropertySelect, PropertySlider } from "grapesjs";
-import { MAIN_BORDER_COLOR, ROUND_BORDER_COLOR, cx } from "../common";
-import { useEditor } from "@grapesjs/react";
+import type { Property, PropertyComposite, PropertyRadio, PropertySelect, PropertySlider, PropertyStack } from "grapesjs";
+import { ROUND_BORDER_COLOR, cx } from "../common";
 
 interface StylePropertyFieldProps extends React.HTMLProps<HTMLDivElement> {
     prop: Property;
@@ -124,11 +125,50 @@ export default function StylePropertyField({ prop, ...rest }: StylePropertyField
             const compositeProp = prop as PropertyComposite;
             inputToRender = (
                 <div className={cx('flex flex-wrap p-2 bg-black/20', ROUND_BORDER_COLOR)}>
-                    {
-                        compositeProp.getProperties().map(prop => (
-                            <StylePropertyField key={prop.getId()} prop={prop} />
-                        ))
-                    }
+                    {compositeProp.getProperties().map(prop => (
+                        <StylePropertyField key={prop.getId()} prop={prop} />
+                    ))}
+                </div>
+            );
+        } break;
+        case 'stack': {
+            const stackProp = prop as PropertyStack;
+            const layers = stackProp.getLayers();
+            const isTextShadow = stackProp.getName() === 'text-shadow';
+            inputToRender = (
+                <div className={cx('flex flex-col p-2 gap-2 bg-black/20 min-h-[54px]', ROUND_BORDER_COLOR)}>
+                    {layers.map(layer => (
+                            <div key={layer.getId()} className={ROUND_BORDER_COLOR}>
+                                <div className="flex gap-1 bg-slate-800 px-2 py-1 items-center">
+                                    <IconButton size="small" onClick={() => layer.move(layer.getIndex() - 1)}>
+                                        <Icon size={0.7} path={mdiArrowUpDropCircle}/>
+                                    </IconButton>
+                                    <IconButton size="small" onClick={() => layer.move(layer.getIndex() + 1)}>
+                                        <Icon size={0.7} path={mdiArrowDownDropCircle}/>
+                                    </IconButton>
+                                    <button className="flex-grow" onClick={() => layer.select()}>
+                                        { layer.getLabel() }
+                                    </button>
+                                    <div
+                                        className={cx('bg-white min-w-[17px] min-h-[17px] text-black text-sm flex justify-center', ROUND_BORDER_COLOR)}
+                                        style={layer.getStylePreview({ number: { min: -3, max: 3 } })}
+                                    >
+                                        { isTextShadow && 'T' }
+                                    </div>
+                                    <IconButton size="small" onClick={() => layer.remove()}>
+                                        <Icon size={0.7} path={mdiDelete}/>
+                                    </IconButton>
+                                </div>
+                                {
+                                    layer.isSelected() &&
+                                    <div className="p-2 flex flex-wrap">
+                                        {stackProp.getProperties().map(prop => (
+                                            <StylePropertyField key={prop.getId()} prop={prop} />
+                                        ))}
+                                    </div>
+                                }
+                            </div>
+                    ))}
                 </div>
             );
         } break;
@@ -136,13 +176,23 @@ export default function StylePropertyField({ prop, ...rest }: StylePropertyField
 
     return (
         <div {...rest} className={cx('mb-3 px-1', prop.isFull() ? 'w-full' : 'w-1/2')} >
-            <div className={cx('flex mb-2', canClear && 'text-sky-300')}>
+            <div className={cx('flex mb-2 items-center', canClear && 'text-sky-300')}>
                 <div className="flex-grow capitalize">{ prop.getLabel() }</div>
                 {
                     canClear &&
                     <button onClick={() => prop.clear()}>
                         <Icon path={mdiClose} size={0.7}/>
                     </button>
+                }
+                {
+                    type === 'stack' &&
+                    <IconButton
+                        size="small"
+                        className="!ml-2"
+                        onClick={() => (prop as PropertyStack).addLayer({}, { at: 0 })}
+                    >
+                        <Icon size={1} path={mdiPlus}/>
+                    </IconButton>
                 }
             </div>
             { inputToRender }
