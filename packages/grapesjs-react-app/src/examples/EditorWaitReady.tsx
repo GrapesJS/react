91188516e09e54a2f72cafbe3bf86c50583c597a
Artur Arseniev <artur.catch@hotmail.it>
2023-07-03 12:49:23 +0400

    Up

--- packages/grapesjs-react-app/src/examples/EditorWaitReady.tsx
+++ packages/grapesjs-react-app/src/examples/EditorWaitReady.tsx
@@ -2,27 +2,31 @@ import GrapesJsEditor, { EditorProps } from '@grapesjs/react';
 import { defaultEditorProps, slowStoragePlugin } from './common';
 import CircularProgress from '@mui/material/CircularProgress';
 
+const options = {
+  ...defaultEditorProps.options,
+  storageManager: { type: 'slow' }
+};
+
+const plugins = [
+  slowStoragePlugin,
+  ...defaultEditorProps.plugins!,
+];
+
+const Spinner = (
+  <div className="flex items-center h-full justify-center">
+    <CircularProgress/>
+  </div>
+)
+
 export default function EditorWaitReady(props: Partial<EditorProps>) {
     return (
         <GrapesJsEditor
           className="gjs-editor-wait-ready"
           {...defaultEditorProps}
-          options={{
-            ...defaultEditorProps.options,
-            storageManager: {
-              type: 'slow'
-            }
-          }}
-          plugins={[
-            slowStoragePlugin,
-            ...defaultEditorProps.plugins!,
-          ]}
+          options={options}
+          plugins={plugins}
           {...props}
-          waitReady={
-            <div className="flex items-center h-full justify-center">
-              <CircularProgress/>
-            </div>
-          }
+          waitReady={Spinner}
         />
     )
 }
