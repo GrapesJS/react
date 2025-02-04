8bf3ebec6e91a3316398d58d0c669598f987f842
danstarns <danielstarns@hotmail.com>
2025-02-03 17:23:51 -0800

    package: updates *

--- packages/grapesjs-react-app-19/src/main.tsx
+++ packages/grapesjs-react-app-19/src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css';
+import { StrictMode } from 'react';
 
 const root = document.getElementById('root') as HTMLElement
 
 const reactRoot = createRoot(root)
 
-reactRoot.render( <App />);
+reactRoot.render(  <StrictMode>
+    <App />
+  </StrictMode>);
 
